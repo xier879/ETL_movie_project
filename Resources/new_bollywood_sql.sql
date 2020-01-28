6a1df9eeac49cc39e2376e37d91d8cbf03edcca6
Xier Zhao <zhaox310@gmail.com>
2020-01-27 21:02:20 -0500

    update bollywood dataset

--- Resources/new_bollywood_sql.sql
+++ Resources/new_bollywood_sql.sql
@@ -1,17 +1,14 @@
+--'imdbId', 'title', 'releaseYear', 'hitFlop'
 -- Create tables for raw data to be loaded into
---DROP TABLE Bollywood_movie
+DROP TABLE Bollywood_movie
 
 CREATE TABLE Bollywood_movie (
+
 "imdbId" TEXT,
 "title" TEXT,
 "releaseYear" VARCHAR,
-"releaseDate" DATE,
-"genre" VARCHAR,
-"writers" TEXT,
-"actors" TEXT,
-"directors" TEXT,
-"sequel" FLOAT8,
 "hitFlop" INT
 );
 
 SELECT * FROM Bollywood_movie
+
